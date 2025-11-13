3c0d8ac8d9344a1c1e8f5ee669a4ce6f5c5db190
Siddharth-Jhingran <siddharthjhingran29@gmail.com>
2025-11-14 03:16:28 +0530

    Add allowedHosts configuration for Vite server

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ['movix-teb2.onrender.com'] // <-- Bas yeh line add karni hai
+  }
+
 })
